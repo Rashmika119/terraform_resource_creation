483a7a9d5c6c1b6215fcfd6a8d9a3962da4cbca4
rashmika <rashmikanethsarani119@gmail.com>
2025-06-20 22:34:00 +0530

    created route table association resource

---
+++ modules/aws_route_table_association/variables.tf
@@ -0,0 +1,8 @@
+variable "route_table_id"{
+    description = "The ID of the route table to associate with the subnet"
+    type        = string
+}
+variable "subnet_id"{
+    description = "The ID of the subnet to associate with the route table"
+    type        = string
+}
