fb300e9fb7d0e3523bf2e206595035e3435f7246
rashmika <rashmikanethsarani119@gmail.com>
2025-06-21 16:57:29 +0530

    modified the key pair value files

--- modules/key_pair/variables.tf
+++ modules/key_pair/variables.tf
@@ -2,7 +2,7 @@ variable "key_name_value"{
     description="value for the key name"
 }
 
-variable  "public_key_value"{
+variable "public_key_value"{
     description="value for the public key"
 }
 
